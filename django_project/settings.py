016a61c6bd5de5374fa77c0aaf8a922029577723
Ricky Oswald <rickyoswald@gmail.com>
2025-01-27 13:43:07 +0000

    Revert "Adding favicon"
    This reverts commit c9da72122c598fb1af1038c2c27d18ed5db27535.

--- django_project/settings.py
+++ django_project/settings.py
@@ -139,6 +139,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-STATIC_ROOT = '/home/rickyoswald/django_project/static'
-STATIC_URL = '/static/'
